cd08a6883f05408e640088907e58e20606d1cda8
Rachel Xiao <rachelxiao907@gmail.com>
2021-01-06 12:11:46 -0500

    Created set methods

--- Node.java
+++ Node.java
@@ -1,17 +1,30 @@
 public class Node {
   private String data;
   private Node next, prev;
+
   public Node(String value) {
     data = value;
   }
+
   public String getData() {
     return data;
   }
+  public void setData(String value) {
+    data = value;
+  }
+
   public Node getNext() {
     return next;
   }
+  public void setNext(Node value) {
+    next = value;
+  }
+
   public Node getPrev() {
     return prev;
+  }
+  public void setPrev(Node value) {
+    prev = value;
   }
 
 }
