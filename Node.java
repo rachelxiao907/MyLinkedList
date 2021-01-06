0679be8358f54c82d4c84f5ed904dc0dbb035616
Rachel Xiao <rachelxiao907@gmail.com>
2021-01-06 12:07:18 -0500

    Created constructor and get methods

---
+++ Node.java
@@ -0,0 +1,17 @@
+public class Node {
+  private String data;
+  private Node next, prev;
+  public Node(String value) {
+    data = value;
+  }
+  public String getData() {
+    return data;
+  }
+  public Node getNext() {
+    return next;
+  }
+  public Node getPrev() {
+    return prev;
+  }
+
+}
